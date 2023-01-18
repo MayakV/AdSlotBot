f9db2f91430050857b000d8d4dfca10e5b4123f4
Sharov Dmitry <dsharov96@gmail.com>
2023-01-18 17:05:01 +0400

    Fixing update_db.pyto work with rearranged modules

--- expire_script/update_db.py
+++ expire_script/update_db.py
@@ -1,13 +1,18 @@
 import os
 import sys
 import datetime
-
+# needed to import modules below
 parent_dir = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
+# print(parent_dir)
 sys.path.insert(0, parent_dir)
 
-import db
+import shared.db as db
 
-conn = db.Connection()
+db_name = os.getenv("DB_NAME")  # configParser.get('General', 'Db_name')
+host = os.getenv("HOSTNAME")   #configParser.get('General', 'Host')
+port = int(os.getenv("PORT"))
+
+conn = db.Connection(host, port, db_name)
 
 # conn.update_expired_users()
 print("Expired users updated : " + str(conn.update_expired_users()))
