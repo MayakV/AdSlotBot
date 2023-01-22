7e95b4989e0dff455d46687ecf1af7156349879e
Sharov Dmitry <dsharov96@gmail.com>
2023-01-23 01:34:49 +0400

    Added execute permission on cron_setup

--- scraper/cron_setup.sh
+++ scraper/cron_setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Looks ugly bot works
+# echo "*/5 * * * * root HOSTNAME=$HOSTNAME PORT=$PORT DB_NAME=$DB_NAME python -u /bin/update_db.py > /proc/1/fd/1 2>/proc/1/fd/2" >> /etc/crontab
+# The line above writes cron instruction in crontab
+# The intsruction is launching /bin/update_db.py with python every 5 minutes under root user with soe environment variables
+# Output from said command redirected to console -u flag in python is needed for docker to see console output
+# && cron -f
+# After writing in crontab it launches cron in foreground so that container wouldn't stop automaticaly
 echo "0 * * * * root " \
 "HOSTNAME=$HOSTNAME " \
 "PORT=$PORT " \
